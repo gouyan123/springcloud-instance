b265b1f5e140fdf2fe585e721dedc7b0249a8303
gouyan <2637178209@qq.com>
2018-10-11 10:30:40 +0800

    gx

--- uaa-interface/src/main/java/com/dongnaoedu/springcloud/uaa/web/TokenController.java
+++ uaa-interface/src/main/java/com/dongnaoedu/springcloud/uaa/web/TokenController.java
@@ -2,7 +2,6 @@ package com.dongnaoedu.springcloud.uaa.web;
 
 import java.util.Date;
 import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.dongnaoedu.springcloud.uaa.db.UserDomain;
 import com.dongnaoedu.springcloud.uaa.db.UserRepository;
 import com.dongnaoedu.springcloud.uaa.jwt.JwtConfiguration;
 import com.dongnaoedu.springcloud.uaa.jwt.JwtTokenProvider;
 import com.dongnaoedu.springcloud.uaa.jwt.UAAClaims;
-
 import io.jsonwebtoken.Claims;
 
 /**
